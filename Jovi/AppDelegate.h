00adfbb16fd728dfa4437333e9a98a253c12190c
shakked <zshakked@stevens.edu>
2015-04-15 16:07:31 -0400

    Initial Commit

---
+++ Jovi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jovi
+//
+//  Created by Zachary Shakked on 4/15/15.
+//  Copyright (c) 2015 Jovi LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
